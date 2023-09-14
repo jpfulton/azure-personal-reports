1713629978d609c85e21d670201d4b48adfe6246
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-09-14 16:05:12 -0500

    Update to better handle spaces in resource names and generate a summary README.

--- .github/scripts/accumulated-cost-by-resource.sh
+++ .github/scripts/accumulated-cost-by-resource.sh
@@ -3,30 +3,76 @@
 OUTPUT="";
 COST_REPORTS_DIR="./cost-reports";
 
+SUMMARY_FILE="${COST_REPORTS_DIR}/README.md";
+
+# Pre-filter some cost-less resource types and sort by resource name
+JMES_QUERY="
+sort_by(
+  [?
+    type != 'Microsoft.Compute/sshPublicKeys' &&
+    type != 'Microsoft.Compute/virtualMachines/extensions' &&
+    type != 'Microsoft.Network/networkInterfaces' &&
+    type != 'Microsoft.Network/networkSecurityGroups'
+  ].{id: id, name: name},
+  &name) | [].id
+";
+
+echo "# Resource Costs Index" > $SUMMARY_FILE;
+echo "" >> $SUMMARY_FILE;
+
+echo "- [Total Accumulated Costs](./accumulated-cost.md)" >> $SUMMARY_FILE;
+echo "- [Total Cost by Resource](./cost-by-resource.md)" >> $SUMMARY_FILE;
+echo "- [Total Daily Costs](./daily-costs.md)" >> $SUMMARY_FILE;
+echo "" >> $SUMMARY_FILE;
+
 echo "Fetching resource groups...";
-RESOURCE_GROUPS=( $(az group list --query "[].name" --output tsv ) );
+RESOURCE_GROUPS=( $(az group list --query "sort_by([].{name: name}, &name)" --output tsv ) );
 
 for rg in ${RESOURCE_GROUPS[@]}; do
-  echo "Fetching resources for group ${rg}...";
-  RESOURCES_IDS=( $(az resource list --resource-group $rg --query "[].id" --output tsv) );
+  echo "## ${rg}" >> $SUMMARY_FILE;
+  echo "" >> $SUMMARY_FILE;
 
   RG_DIR="${COST_REPORTS_DIR}/${rg}";
   mkdir -p $RG_DIR;
+
+  echo "Build resource group level cost report for group ${rg}...";
+  OUTPUT=$(azure-cost accumulatedCost --filter "ResourceGroupName=$rg" --output Markdown)
+  if [ $? -eq 0 ];
+    then
+      echo "- [Resource Group Summary](./${rg}/resource-group-summary.md)" >> $SUMMARY_FILE;
+
+      echo "Writing accumulated cost report for ${rg}.";
+      echo "$OUTPUT" > ${RG_DIR}/resource-group-summary.md; 
+    else
+      echo "Error running accumulated cost report for group: ${rg}.";
+      echo "---";
+  fi
+
+  echo "Fetching resources for group ${rg}...";
+  #RESOURCES_IDS=( $(az resource list --resource-group $rg --query "$JMES_QUERY" --output tsv) );
+  readarray -t RESOURCES_IDS < <(az resource list --resource-group $rg --query "$JMES_QUERY" --output tsv);
 
   for id in ${RESOURCES_IDS[@]}; do
     RESOURCE_NAME=$(echo $id | rev | cut -d "/" -f 1 | rev);
     OUTPUT_FILE_NAME="${RG_DIR}/${RESOURCE_NAME}.md";
 
     echo "Building cost report for ${RESOURCE_NAME} in group ${rg}...";
-    OUTPUT=$(azure-cost accumulatedCost --filter ResourceId=$id --output Markdown);
+    OUTPUT=$(azure-cost accumulatedCost --filter "ResourceId=$id" --output Markdown);
 
     if [ $? -eq 0 ]; 
       then
+        echo "- [${RESOURCE_NAME}](./${rg}/${RESOURCE_NAME}.md)" >> $SUMMARY_FILE;
+
         echo "Writing accumulated cost report for ${RESOURCE_NAME} in group ${rg}.";
         echo "$OUTPUT" > $OUTPUT_FILE_NAME; 
       else
         echo "Error running accumulated cost report for ${RESOURCE_NAME} in group: ${rg}.";
+        echo "Resource ID: ${id}";
+        echo "---";
     fi
   done
 
+  echo "" >> $SUMMARY_FILE;
 done
+
+echo "" >> $SUMMARY_FILE;
