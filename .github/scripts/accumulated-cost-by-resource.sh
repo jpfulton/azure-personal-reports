4f706f6ad86e1ce5ab083444f26961296475973c
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-09-14 13:47:06 -0500

    Update with script for costs by resource.

--- .github/scripts/accumulated-cost-by-resource.sh
+++ .github/scripts/accumulated-cost-by-resource.sh
@@ -2,9 +2,11 @@
 
 COST_REPORTS_DIR="./cost-reports";
 
+echo "Fetching resource groups...";
 RESOURCE_GROUPS=( $(az group list --query "[].name" --output tsv ) );
 
 for rg in ${RESOURCE_GROUPS[@]}; do
+  echo "Fetching resources for group: ${rg}...";
   RESOURCES_IDS=( $(az resource list --resource-group $rg --query "[].id" --output tsv) );
 
   RG_DIR="${COST_REPORTS_DIR}/${rg}";
@@ -14,10 +16,16 @@ for rg in ${RESOURCE_GROUPS[@]}; do
     RESOURCE_NAME=$(echo $id | rev | cut -d "/" -f 1 | rev);
     OUTPUT_FILE_NAME="${RG_DIR}/${RESOURCE_NAME}.md";
 
-    #echo $OUTPUT_FILE_NAME;
-
+    echo "Building cost report for ${RESOURCE_NAME} in group: ${rg}...";
     OUTPUT=$(azure-cost accumulatedCost --filter ResourceId=$id --output Markdown);
-    if [ $? -eq 0 ]; then echo "$OUTPUT" > ${RG_DIR}/${name}.md; fi
+
+    if [ $? -eq 0 ]; 
+      then
+        echo "Writing accumulated cost report for ${RESOURCE_NAME} in group ${rg}.";
+        echo "$OUTPUT" > $OUTPUT_FILE_NAME; 
+      else
+        echo "Error running accumulated cost report for ${RESOURCE_NAME} in group: ${rg}.";
+    fi
   done
 
 done
