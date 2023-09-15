7c7a65da9e6f8581d771c35674de609a0879e146
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-09-15 18:28:24 -0500

    Try escaping backslashes.

--- .github/scripts/resource-groups-as-json.sh
+++ .github/scripts/resource-groups-as-json.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-RESOURCE_GROUPS=( $(az group list --query "sort_by([].{name: name}, &name)" --output tsv ) );
-#readarray -t RESOURCE_GROUPS < <(az group list --query "sort_by([].{name: name}, &name)" --output tsv);
+#RESOURCE_GROUPS=( $(az group list --query "sort_by([].{name: name}, &name)" --output tsv ) );
+readarray -t RESOURCE_GROUPS < <(az group list --query "sort_by([].{name: name}, &name)" --output tsv);
 
 echo "{";
-echo "\"group\":";
+echo "\\\"group\\\":";
 echo "[";
 
 for index in ${!RESOURCE_GROUPS[@]}; do
 
-  echo -n "\"${RESOURCE_GROUPS[$index]}\"";
+  echo -n "\\\"${RESOURCE_GROUPS[$index]}\\\"";
 
   if [ "$(($index + 1))" -ne "${#RESOURCE_GROUPS[@]}" ];
     then
